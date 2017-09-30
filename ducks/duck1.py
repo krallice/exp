2a866ee9cf137e7847043728a0805f603d6256f7
krallice <sroz9960@gmail.com>
2017-09-30 05:49:25 -0400

    Duck1

---
+++ ducks/duck1.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+### Flying Class ###
+class FlyBehavior(object):
+
+	def fly(self):
+		pass
+
+class FlyWithWings(FlyBehavior):
+
+	def fly(self):
+		print "Im Flying!!"
+
+class FlyNoWay(FlyBehavior):
+
+	def fly(self):
+		print "I cant fly brazz"
+
+### Quacks ###
+class QuackBehavior(object):
+
+	def quack(self):
+		pass
+
+class Quack(QuackBehavior):
+
+	def quack(self):
+		print "Quack"
+
+class MuteQuack(QuackBehavior):
+	
+	def quack(self):
+		print "Silence..."
+
+class Squeak(QuackBehavior):
+
+	def quack(self):
+		print "SQUEAK"
+
+### Duck ###
+class Duck(object):
+	
+	def __init__(self):
+		self.flyBehavior = FlyBehavior()
+		self.quackBehavior = QuackBehavior()
+
+	def performQuack(self):
+		self.quackBehavior.quack()
+
+	def performFly(self):
+		self.flyBehavior.fly()
+
+	def swim(self):
+		print "All ducks float, even decoys"
+
+class MallardDuck(Duck):
+
+	def __init__(self):
+		self.quackBehavior = Quack()
+		self.flyBehavior = FlyWithWings()
+
+class ModelDuck(Duck):
+
+	def __init__(self):
+		self.quackBehavior = Quack()
+		self.flyBehavior = FlyNoWay()
+
+### Main ###
+if __name__ == "__main__":
+	print "Strategy"
+	md = MallardDuck()
+	md.performFly()
+	md.performQuack()
+	
+	model = ModelDuck()
+	model.performFly()
