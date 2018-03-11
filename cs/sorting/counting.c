e3a13c7dbb686c26474d19abaffb7e874fefca28
krallice <sroz9960@gmail.com>
2018-03-11 10:39:43 +0000

    Counting sort in C

--- cs/sorting/counting.c
+++ cs/sorting/counting.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ARRAY_SIZE 8
-
-#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 
 void dump_array(int ar[], int size) {
 	for (int i = 0; i < size; i++) {
@@ -12,12 +11,48 @@ void dump_array(int ar[], int size) {
 	printf("\n");
 }
 
+void counting_sort(int ar[], int size, int sorted[], int maxvalue) {
+
+	// Init our counting index to 0;
+	int c[maxvalue + 1];
+	for (int i = 0; i <= maxvalue; i++) {
+		c[i] = 0;
+	}
+
+	// Create a histogram, each value in ar,
+	// increment the matching index value in the histogram array:
+	for (int j = 0; j < size; j++) {
+		c[ar[j]] = c[ar[j]] + 1;
+	}
+
+	dump_array(c, maxvalue + 1);
+	printf("\n");
+
+	// Loop through our histogram:
+	int sorted_index = 0;
+	for (int i = 0; i < maxvalue + 1; i++) {
+		// A positive value in the histogram:
+		while (c[i] > 0) {
+			// Write this value to our sorted array, and increment the index:
+			sorted[sorted_index++] = i;
+			// Deplete the histogram's value:
+			c[i]--;
+		}
+	}
+}
+
 int main(void) {
 
-	int ar[ARRAY_SIZE] = { 1, 19, 254, 11, 25, 43, 12, 3 };
+	int ar[ARRAY_SIZE] = { 1, 19, 254, 11, 25, 11, 12, 3 };
 	int arsize = sizeof(ar)/sizeof(int);
 
+	int sorted[arsize];
+	int maxvalue = 254;
+
 	dump_array(ar, arsize);
+	counting_sort(ar, arsize, sorted, maxvalue);
+	printf("\n");
+	dump_array(sorted, arsize);
 
 	return 0;
 }
